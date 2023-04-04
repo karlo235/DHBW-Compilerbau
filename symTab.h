623be8ec558941e58c3f8e44893333b669b10c4f
Karlo Ivanovic <ivanovickarlo213@gmail.com>
2023-04-05 00:31:03 +0200

    Code formatting

--- symTab.h
+++ symTab.h
@@ -1,26 +1,29 @@
 #ifndef SYMTAB_H
 #define SYMTAB_H
+
 #include <stdio.h>
 
 // Possible types in Predicate Logic 1
-enum type{Predicate, Function, Variable};
+enum type {
+    Predicate, Function, Variable
+};
 typedef enum type type;
 
 // Datatype of a symbol table entry
-struct tableEntry_struct{
-	char identifier[101];
-	type type;
-	// Number of arguments
-	int arity;
-	// Pointer on the next element
-	struct tableEntry_struct *next;
+struct tableEntry_struct {
+    char identifier[101];
+    type type;
+    // Number of arguments
+    int arity;
+    // Pointer on the next element
+    struct tableEntry_struct *next;
 };
 
-typedef struct tableEntry_struct* tableEntry;
+typedef struct tableEntry_struct *tableEntry;
 
 tableEntry addSymbolEntry(char identifier[], type type, int arity);
 
-tableEntry getSymbolEntry(char* identifier);
+tableEntry getSymbolEntry(char *identifier);
 
 int checkSymbol(char identifier[], type type);
 
